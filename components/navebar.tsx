a72f5b5c6bdc6e66e3c47ac50bdb648a11b1f863
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-06-30 14:35:50 +0530

    feat:setup basic nav bar

--- components/navebar.tsx
+++ components/navebar.tsx
@@ -18,8 +18,27 @@ export const Navbar = () => {
         setNav(!nav)
     }
 
+    const closeNav = () => {
+        setNav(false)
+    }
+
     return(
-        <>
-        </>
+        <div className="z=50 fixed flex justify-center w-full text-white font-bold">
+
+            <div className="border border-white/20 mt-8 backdrop-blur-3xl rounded-3xl
+                            hidden md:flex items-center justify-center p-2 max-w-[400px] mx-auto">
+                <ul className="flex flex-row p-2 space-x-8">
+                    {navLinks.map((link, index) => (
+                        <li key={index}>
+                            <Link href={link.path} className="transform hover:text-white/50
+                                                             transition-all duration-300 ease-in-out">
+                                {link.title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+        </div>
     )
 }
