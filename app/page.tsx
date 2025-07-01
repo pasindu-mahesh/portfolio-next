a629426da5eb51f941b70e1b2abac7756ba27678
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-02 01:38:48 +0530

    feat: logo animation compleat.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import { Hero } from "@/components/hero";
 import { Navbar } from "@/components/navbar";
 import { Stack } from "@/components/stack";
+import { LogoAnimation } from "@/components/logoAnimation";
+
 
 export default function Home() {
   return (
     <>
+
       <Navbar/>
       <Hero/>
       <Stack/>
+      <LogoAnimation/>
       
       
     </>
--- components/logoAnimation.tsx
+++ components/logoAnimation.tsx
@@ -32,5 +32,40 @@ const images = [
 ]
 
 export const LogoAnimation = () => {
+    return (
+        <div className="py-9 bg-purple-200/10 opacity-80 glass">
+            <div className="container mx-auto">
+                <div className="overflow-hidden
+                        [mask-image:linear-gradient(to_right_transparent,_black_25%_black_75%,_transparent)]">
+                <motion.div
+                    className="flex gap-14 flex-none pr-14"
+                    animate={{
+                        translateX: '-50%',
+                    }}
+                    transition={{
+                        duration: 35,
+                        repeat: Infinity,
+                        ease: 'linear',
+                        repeatType: 'loop',
+                    }}
+                >
+                    {images.map((image, index) => (
+                        <Image
+                            key={index}
+                            src={image.src}
+                            alt={image.alt}
+                            height={30}
+                        />
 
+                    ))}
+
+                </motion.div>
+
+                </div>
+
+            </div>
+
+
+        </div>
+    );
 }
