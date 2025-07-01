e73196fde2c473338ec668551b6622b1db77b244
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-02 00:02:07 +0530

    feat: creat logo animation sectin img arry

---
+++ components/logoAnimation.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import Image from "next/image"
+import { motion } from "framer-motion"
+import udemy from "@/assets/udemy.png"
+import fiver from "@/assets/fiverr.png"
+import elementor from "@/assets/elementor.png"
+import logitech from "@/assets/logitech.png"
+import oracle from "@/assets/oracle.png"
+
+const images = [
+    {src: udemy, alt: "Udemy"},
+    {src: fiver, alt: "Fiverr"},
+    {src: elementor, alt: "Elementor"},
+    {src: logitech, alt: "Logitech"},
+    {src: oracle, alt: "Oracle"},
+    {src: udemy, alt: "Udemy"},
+    {src: fiver, alt: "Fiverr"},
+    {src: elementor, alt: "Elementor"},
+    {src: logitech, alt: "Logitech"},
+    {src: oracle, alt: "Oracle"},
+    {src: udemy, alt: "Udemy"},
+    {src: fiver, alt: "Fiverr"},
+    {src: elementor, alt: "Elementor"},
+    {src: logitech, alt: "Logitech"},
+    {src: oracle, alt: "Oracle"},
+    {src: udemy, alt: "Udemy"},
+    {src: fiver, alt: "Fiverr"},
+    {src: elementor, alt: "Elementor"},
+    {src: logitech, alt: "Logitech"},
+    {src: oracle, alt: "Oracle"},
+]
+
+export const LogoAnimation = () => {
+
+}
