bbdc665c77bcc2f1869f545a699e4807d81f728f
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-01 20:22:15 +0530

    feat:add stack items

--- components/stack.tsx
+++ components/stack.tsx
@@ -5,6 +5,14 @@ import { SiTypescript } from 'react-icons/si';
 import { SiMongodb } from 'react-icons/si';
 import { TbBrandNextjs } from 'react-icons/tb';
 
+const stackItems = [
+    { id: 1, name: 'React', icon: FaReact, color: '#61DAFB'},
+    { id: 2, name: 'Node.js', icon: IoLogoNodejs, color: '#339933'},
+    { id: 3, name: 'TypeScript', icon: SiTypescript, color: '#3178C6'},
+    { id: 4, name: 'MongoDB', icon: SiMongodb, color: '#47A248'},
+    { id: 5, name: 'Next.js', icon: TbBrandNextjs, color: '#000000'}
+]
+
 export const Stack = () => {
     return (<></>)
 }
