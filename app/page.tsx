9edb20ccd22aceb5e50602021eb786c3282d0e98
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-01 20:14:42 +0530

    feat: import icons for stack section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Hero } from "@/components/hero";
 import { Navbar } from "@/components/navbar";
-
+import { Stack } from "@/components/stack";
 
 export default function Home() {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <Stack/>
       
       
     </>
---
+++ components/stack.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FaReact } from 'react-icons/fa';
+import { IoLogoNodejs } from 'react-icons/io';
+import { SiTypescript } from 'react-icons/si';
+import { SiMongodb } from 'react-icons/si';
+import { TbBrandNextjs } from 'react-icons/tb';
+
+export const Stack = () => {
+    return (<></>)
+}
