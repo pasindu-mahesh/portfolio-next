d9e261c6a63e0b92174758aa7fac26d53fb9e188
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-01 23:50:35 +0530

    feat:  compleat my stack section

--- components/stack.tsx
+++ components/stack.tsx
@@ -15,7 +15,7 @@ const stackItems = [
 
 export const Stack = () => {
     return (
-        <section className="py-6">
+        <section className="py-6 glass">
             <div className="max-w-[1200px] mx-auto px-4 text-center">
                 <h2 className="text-5xl text-gray-200 font-bold mb-4">My Stack</h2>
                 <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2">
@@ -24,12 +24,13 @@ export const Stack = () => {
                             key={item.id}
                             className="flex items-center justify-center flex-col rounded-xl p-4">
 
-                                <div>
+                                <div className="mb-4 bg-white/10 p-6 rounded-xl">
                                     {React.createElement(item.icon, {
                                         className:"w-32 h-32",
                                         style: {color: item.color}
                                     })}
                                 </div>
+                                <p className="text-gray-400 font-semibold">{item.name}</p>
 
                         </div>
                     ))}
