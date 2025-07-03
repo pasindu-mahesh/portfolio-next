5f25267d2a70ec6e6f73a66c362c89f90748ea09
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-03 14:59:16 +0530

    feat: add map for contacts section

--- components/contacts.tsx
+++ components/contacts.tsx
@@ -58,10 +58,25 @@ export const Contacts = () => {
                                 Malabe, <br/>
                                 Sri Lanka.
                             </address>
-
                         </div>
                     </motion.div>
                 </div>
+
+                <motion.div
+                    initial={{ opacity: 0, x: 20 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.6, delay: 0.6 }}
+                    className="w-full h-full min-h-400 rounded-2xl overflow-hidden"
+                >
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6261.000744339571!2d79.94915973796185!3d6.910826942264336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae257255ecc9693%3A0xca4b7f07244fbd91!2sSamanala%20Mawatha%2C%20Malabe!5e1!3m2!1sen!2slk!4v1751534391425!5m2!1sen!2slk"
+                        style={{ border: 0, width: "100%", height: "100%" }}
+                        allowFullScreen
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    >
+                    </iframe>
+                </motion.div>
             </motion.div>
         </section>
     );
