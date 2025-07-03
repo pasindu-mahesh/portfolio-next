e2333a209436b4b042c1c7cb44b6e789a76b443c
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-07-03 12:57:36 +0530

    feat: contatcs motion div

--- components/contacts.tsx
+++ components/contacts.tsx
@@ -4,6 +4,21 @@ import { motion } from "framer-motion";
 
 export const Contacts = () => {
     return (
-        <></>
+        <section id="contacts" className="py-32 text-white max-w-[1200px] mx-auto px-4">
+            <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8 }}
+                viewport={{ once: true }}
+                className="grid lg:grid-cols-2 gap-16"
+            >
+                <div className="space-y-12">
+
+
+                </div>
+
+            </motion.div>
+
+        </section>
     );
 };
